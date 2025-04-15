b08bfc8e39a7dd28ace884daef2cc47eeb5685d9
unknown <e1509819@n.nus.edu>
2025-04-15 09:15:43 +0800

    tf test2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,21 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 3.0" # Specify a version constraint (optional but recommended)
+    }
+  }
+}
+
 provider "digitalocean" {
-  token = var.digitalocean_token 
+  token = var.digitalocean_token
 }
 
 resource "digitalocean_droplet" "app_server" {
-  name    = "tradeport-server"
-  region  = "sgp1"
-  size    = "s-2vcpu-4gb"
-  image   = "ubuntu-22-04-x64"
+  name   = "tradeport-server"
+  region = "sgp1"
+  size   = "s-2vcpu-4gb"
+  image  = "ubuntu-22-04-x64"
   ssh_keys = [var.ssh_fingerprint]
 }
 
