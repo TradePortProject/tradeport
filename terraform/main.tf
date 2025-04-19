1cf21ef1c1a5c5a984a60640152b0c008f3eb8b6
e1509819 <e1509819@u.nus.edu>
2025-04-19 16:52:38 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,20 @@
+terraform {
+    required_providers {
+        docker = {
+            source = "kreuzwerker/docker"
+            version = "3.0.2"
+        }
+        digitalocean = {
+            source = "digitalocean/digitalocean"
+            version = "2.26.0"
+        }
+        local = {
+            source = "hashicorp/local"
+            version = "2.4.0"
+        }
+    }
+}
+
 data "digitalocean_droplet" "app_server" {
   name = "tradeport-server"
   # Add a depends_on to ensure this runs after a potential creation
