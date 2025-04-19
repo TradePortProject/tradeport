1369f0219b7ac907d8fe2dd9ca6b8af75927f413
e1509819 <e1509819@u.nus.edu>
2025-04-19 16:50:25 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,19 @@
-terraform {
-    required_providers {
-        docker = {
-            source = "kreuzwerker/docker"
-            version = "3.0.2"
-        }
-        digitalocean = {
-            source = "digitalocean/digitalocean"
-            version = "2.26.0"
-        }
-        local = {
-            source = "hashicorp/local"
-            version = "2.4.0"
-        }
-    }
+data "digitalocean_droplet" "app_server" {
+  name = "tradeport-server"
+  # Add a depends_on to ensure this runs after a potential creation
+  # depends_on = [digitalocean_droplet.new_droplet]
 }
 
-provider "digitalocean" {
-  token = var.digitalocean_token
-}
-
-resource "digitalocean_droplet" "app_server" {
-  name       = "tradeport-server-test"
-  region     = "sgp1"
-  size       = "s-2vcpu-4gb"
-  image      = "ubuntu-22-04-x64"
-  ssh_keys   = [var.ssh_fingerprint]
+resource "digitalocean_droplet" "new_droplet" {
+  count       = length(data.digitalocean_droplet.app_server.id) > 0 ? 0 : 1
+  name        = "tradeport-server"
+  region      = "sgp1"
+  size        = "s-2vcpu-4gb"
+  image       = "ubuntu-22-04-x64"
+  ssh_keys    = [var.ssh_fingerprint]
 }
 
 output "droplet_ip" {
   description = "Public IP of the created Droplet"
-  value       = digitalocean_droplet.app_server.ipv4_address
+  value       = length(digitalocean_droplet.new_droplet) > 0 ? digitalocean_droplet.new_droplet[0].ipv4_address : data.digitalocean_droplet.app_server.ipv4_address
 }
